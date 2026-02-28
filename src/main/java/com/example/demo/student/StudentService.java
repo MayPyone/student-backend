b1dc34203638dd30505887483faf074132f3b0c3
maypyone <maypyone@acfsoft.com>
2026-02-28 15:19:48 +0800

    Add pj

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> student (){
+        return studentRepository.findAll();
+    }
+}
